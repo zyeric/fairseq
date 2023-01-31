bcd9868135e0efaee7b5f7b6f9809d692aa16af2
tntnnlrw <tntnnlrw@163.com>
2023-01-31 06:08:25 +0000

    save work

--- fairseq/criterions/label_smoothed_cross_entropy_latency_augmented.py
+++ fairseq/criterions/label_smoothed_cross_entropy_latency_augmented.py
@@ -3,6 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import sys
+# sys.path.append("/home/yomia/yizhu1/yizhu1_moe/fairseq")
+print(sys.path)
 from examples.simultaneous_translation.utils.latency import LatencyTraining
 from fairseq.criterions import register_criterion
 from fairseq.criterions.label_smoothed_cross_entropy import (
